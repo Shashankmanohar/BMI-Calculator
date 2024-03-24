b4ee6c14aea25ad7c9c78f62294c77841a39d35d
Shashank Manohar <146476769+Shashankmanohar@users.noreply.github.com>
2024-03-24 10:57:21 +0530

    Add files via upload
    This is my 2 day challenge of making project.
    Today i have created a BMI calculator. I have also attached all the files you can check it.

---
+++ app.js
@@ -0,0 +1,28 @@
+let calculatebtn = document.querySelector(".Calculatebtn");
+
+calculatebtn.addEventListener('click',calculate);
+
+function calculate(){
+    let Weight = document.getElementById("weight").value;  //get the value of weight from
+    let Height = document.getElementById("height").value;
+    if(Weight.value == "" || Height.value == ""){
+        alert("Please fill out all fields!");
+    }else {
+    let BMI = (Weight/(Height * Height)).toFixed(2);
+    let result = `Your BMI is: ${BMI}`;
+    if(BMI < 18.5){
+        result += "(Underweight)";
+    }else if(BMI >= 18.5 &&  BMI <= 25){
+        result += "(Normal Weight)";
+    }else if(BMI >= 25 &&  BMI <= 30){
+        result += "(Overweight)";
+    }else if(BMI >= 30 &&   BMI <= 40){
+        result += "(Obese)";
+    }
+    document.getElementsByClassName( 'result' )[0].textContent= result;
+    document.getElementById("weight").value ="";
+    document.getElementById("height").value ="";
+    //clear input field after calculation
+    };
+    
+}
